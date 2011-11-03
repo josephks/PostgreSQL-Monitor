bdaea07c9da425ae8a1c9f55fbb3b80671a17400
Joseph Shraibman <jks@iname.com>
2011-11-03 13:55:14 -0400

    Updating web plugin def.

--- project/LiftProject.scala
+++ project/LiftProject.scala
@@ -22,7 +22,7 @@ object MyBuild extends Build{
     libraryDependencies ++= Seq(
       "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
       "net.liftweb" %% "lift-mapper" % liftVersion % "compile",
-      "org.mortbay.jetty" % "jetty" % "6.1.26" % "jetty",
+      "org.mortbay.jetty" % "jetty" % "6.1.26" % "container",
       "junit" % "junit" % "4.7" % "test",
       "ch.qos.logback" % "logback-classic" % "0.9.26",
       "org.scala-tools.testing" %% "specs" % "1.6.9" % "test",
---
+++ project/plugins/plugins.sbt
@@ -0,0 +1,4 @@
+libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-web-plugin" % (v+"-0.2.8"))
+
+
+//see: https://github.com/siasia/xsbt-web-plugin
