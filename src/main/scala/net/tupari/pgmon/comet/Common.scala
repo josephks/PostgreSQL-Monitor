4d942a8c18386144cb44332f058f55f733213e30
Joseph Shraibman <jks@iname.com>
2012-03-29 23:57:45 -0400

    Added DataFetcher trait

--- src/main/scala/net/tupari/pgmon/comet/Common.scala
+++ src/main/scala/net/tupari/pgmon/comet/Common.scala
@@ -14,21 +14,14 @@ import net.liftweb.db.{DefaultConnectionIdentifier, ConnectionIdentifier}
 
 object Common {
 
-  //returns an either (tuple, error string)
-    def getData(q: String, target_host: String = null, cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ] = {
-      val connectionIdentifier = Implicits.stringToConnectionIdentifier(target_host)
-      getDataFromConnection(q, Option(connectionIdentifier).getOrElse(DefaultConnectionIdentifier), cur_name)
-    }
-  def getDataFromConnection(q: String, target_host: ConnectionIdentifier , cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ] = {
-    try{
-      Right(net.liftweb.db.DB.performQuery(q, Nil, target_host)   )
-    }catch{
-      //I don't know why but a query that returns no data is considered an error
-      case ex: org.postgresql.util.PSQLException  if (ex.getMessage.contains("No results were returned by the query."))   =>
-        Right(Nil, Nil)
+  var dataFetcher:DataFetcher = new BasicSqlFetcher
 
-    }
-    //todo: catch exception, return Left, or convert to use Box
+  //returns an either (tuple, error string)
+  def getData(q: String, target_host: String = null, cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ] = {
+    dataFetcher.getData(q, target_host, cur_name)
+  }
+  def getDataFromConnection(q: String, target_host: ConnectionIdentifier , cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ] = {
+    dataFetcher.getDataFromConnection(q, target_host, cur_name)
   }
 
   def addResizePluginToFlotWidget(flot_widget_rendered: scala.xml.NodeSeq)={
@@ -46,5 +39,29 @@ object Common {
 
 }
 
+trait DataFetcher {
+  def getData(q: String, target_host: String = null, cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ]
 
+  def getDataFromConnection(q: String, target_host: ConnectionIdentifier , cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ]
+}
+
+class BasicSqlFetcher extends  DataFetcher {
+      //returns an either (tuple, error string)
+    def getData(q: String, target_host: String = null, cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ] = {
+      val connectionIdentifier = Implicits.stringToConnectionIdentifier(target_host)
+      getDataFromConnection(q, Option(connectionIdentifier).getOrElse(DefaultConnectionIdentifier), cur_name)
+    }
+  def getDataFromConnection(q: String, target_host: ConnectionIdentifier , cur_name: String = null): Either[String, Tuple2[ List[String],List[List[Any]] ] ] = {
+    try{
+      Right(net.liftweb.db.DB.performQuery(q, Nil, target_host)   )
+    }catch{
+      //I don't know why but a query that returns no data is considered an error
+      case ex: org.postgresql.util.PSQLException  if (ex.getMessage.contains("No results were returned by the query."))   =>
+        Right(Nil, Nil)
+
+    }
+    //todo: catch exception, return Left, or convert to use Box
+  }
+
+}
 
