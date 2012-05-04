1ff442482ab5eb545ca25ca986ca29cfc1ba41dd
Joseph Shraibman <jks@iname.com>
2012-05-03 22:50:28 -0400

    Added comments

--- src/main/scala/net/tupari/pgmon/comet/UpdateableSpans.scala
+++ src/main/scala/net/tupari/pgmon/comet/UpdateableSpans.scala
@@ -4,17 +4,19 @@ import net.liftweb.http.CometActor
 import net.liftweb.http.js.JsCmds.SetHtml
 
 /**
+ * Mix in to comet actors.
+ * 
  * Created with IntelliJ IDEA.
  * User: jks
  * Date: 5/2/12
  * Time: 3:28 PM
- * To change this template use File | Settings | File Templates.
  */
 
 trait UpdateableSpans {
 
       this: CometActor =>
 
+  /** When parsing html use one of these to create a node that is easy to update via a comet action */
   class UpdateableSpan(val uuid: String){
     def getSpan(node: scala.xml.Node): scala.xml.Elem = <span id={ uuid } >{ node }</span>
     def getSpan: scala.xml.Elem = getSpan(scala.xml.Text("..."))//return a placeholder span to be inserted into html on doc creation
