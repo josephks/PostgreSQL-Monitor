bd41708da9f5f4759ad4c939fb0efef98213b6dc
Joseph Shraibman <jks@iname.com>
2012-05-07 13:29:23 -0400

    No change here, just a figment of git's imagination

---
+++ src/test/scala/net/tupari/pgmon/lib/ImplicitsSpec.scala
@@ -0,0 +1,13 @@
+package net.tupari.pgmon.lib
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jks
+ * Date: 4/20/12
+ * Time: 3:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ImplicitsSpec {
+
+}
